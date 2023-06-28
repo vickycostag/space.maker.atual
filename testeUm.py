99803f64f95d808134ff1f9f817dbfea954653da
vickycostag <vic.victoriacosta@gmail.com>
2023-06-28 15:08:39 -0300

    código quase finalizado

--- testeUm.py
+++ testeUm.py
@@ -1,30 +1,22 @@
-# importações
 import pygame
 import time
 import random
 from tkinter import simpledialog
-
-# coisas do game
 pygame.init()
 tamanho = (1024, 683)
 tamanhoCaixa = (-100, -90)
 tela = pygame.display.set_mode(tamanho)
 clock = pygame.time.Clock()
-cor = (255, 255, 255)
+branco = (255, 255, 255)
 preto = (0,0,0)
 fonte = pygame.font.get_default_font()
 fontepadrao = pygame.font.SysFont(fonte, 30)
 raio = 5
 running = True
-
-# listas
 cordenadas = []
 nomes = []
 linhas = []  
 salvando = []
-
-
-# música, ícone, nome...
 dadosSalvos = False
 fundo = pygame.image.load("fundo.jpg")
 caixaTexto = pygame.image.load("f10.png")
@@ -35,7 +27,6 @@ icone = pygame.image.load("icone.png")
 pygame.display.set_icon(icone)
 pygame.display.set_caption("this is SPACE MAKER!")
 
-# dando início ao game
 while running:
     tela.blit(fundo, (0, 0))
     tela.blit(caixaTexto, (tamanhoCaixa))
@@ -81,16 +72,14 @@ while running:
             time.sleep(2)
         dadosSalvos = False
         
-    #para desenhar as estrelinhas e escrever os nomes
     for i in range(len(cordenadas)):
-        pygame.draw.circle(tela, cor, cordenadas[i], raio)
+        pygame.draw.circle(tela, branco, cordenadas[i], raio)
         pygame.font.init()
-        texto = fontepadrao.render(nomes[i], 1, cor)
+        texto = fontepadrao.render(nomes[i], 1, branco)
         tela.blit(texto, cordenadas[i])
 
-    #para desenhar as linhas
     for linha in linhas:
-        pygame.draw.line(tela, cor, linha[0], linha[1]) 
+        pygame.draw.line(tela, branco, linha[0], linha[1]) 
 
     if len(cordenadas):
         for i in range(len(cordenadas) - 1):
